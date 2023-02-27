f9914953451547149e11babd5f8d9a786fa69aea
TessFanny <fanitess@yahoo.fr>
2023-02-27 12:02:44 +0100

    swagger set up

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const {
   githubRouter
 } = require("./app/router");
 
-const expressJSDocSwagger = require("express-jsdoc-swagger");
+
 
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3001;
 const bodyParser = require('body-parser')
-const helmet = require('helmet'); 
+//const helmet = require('helmet'); 
 const morgan = require('morgan')
 // server initialization
 
@@ -28,12 +28,12 @@ app.use(cors());
 // const filename = fileURLToPath(import.meta.url);
 // const _DirectoryName = path.dirname(filename);
 
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+//app.use(helmet());
+//app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 
 // save HTTP request logging information// of the application in the "common" logging format.
 
-app.use(morgan("common"));
+//app.use(morgan("common"));
 
 // allows the application to handle encoded JSON and URL data 
 
@@ -44,35 +44,15 @@ app.use(bodyParser.urlencoded({limit:"50mb", extended:true}));
 
 // app.use("/assets", express.static(path.join(_DirectoryName, 'public/assets')));
 
-// allow Multer to store uploaded files in the "public/assets" folder on the server keeping their original name.
-
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cb ){
-//         cb (null, "public/assets");
-//     },
-//     filename: function(req, file , cb ){
-//         cb (null, file.originalname);
-//     }
-// });
-
-//const upload = multer({storage});
-
-// REDIRECTION ROUTER
-
-app.use(userRouter);
-app.use(authRouter);
-app.use(postRouter);
-app.use(rssRouter);
-app.use(skillRouter);
-app.use(githubRouter);
-
 
 // SWAGGER
-
+const expressJSDocSwagger = require("express-jsdoc-swagger");
+const expressJSDocSwagger = require("express-jsdoc-swagger");
 const options = {
   info: {
     version: "1.0.0",
     title: "Devboard",
+    description: 'My API with Swagger documentation',
     license: {
       name: "MIT",
     },
@@ -91,6 +71,16 @@ const options = {
 };
 
 expressJSDocSwagger(app)(options);
+// REDIRECTION ROUTER
+
+app.use(userRouter);
+app.use(authRouter);
+app.use(postRouter);
+app.use(rssRouter);
+app.use(skillRouter);
+app.use(githubRouter);
+
+
 
 app.listen(port, () => {
   console.log(`Server ready:  http://localhost:${port}`);
