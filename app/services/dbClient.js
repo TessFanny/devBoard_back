b1e59ca1c2c7b3fd7e183f523b01a4cc5ad52fb9
TessFanny <fanitess@yahoo.fr>
2023-10-19 11:37:06 +0200

    cors setting

--- app/services/dbClient.js
+++ app/services/dbClient.js
@@ -9,5 +9,5 @@ const connectionString = process.env.DB_URL;
 const pool = new Pool({
     connectionString:connectionString
 });
-"PGPASSWORD=nb785AdHLXnKzPSMinPB0tHGAONxTsCD psql -h dpg-ckoekqtih1lc73f3afog-a.frankfurt-postgres.render.com -U devboard devboard_f20j"
+
 module.exports = pool;
