e82718ea32dde4c2e76e1de596f7832147e77f48
TessFanny <fanitess@yahoo.fr>
2023-10-19 11:16:30 +0200

    cors setting

--- index.js
+++ index.js
@@ -21,7 +21,17 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://devboard-api.onrender.com'],
+  credentials: true
+}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://devboard-api.onrender.com');
+ // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  //res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 
